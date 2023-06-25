4709637b19a71b8b752af062df7911c0ae0681ba
kwiatkowskiw <kwiatkowskiww@gmail.com>
2023-06-25 18:59:46 +0200

    change router location

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import "./assets/scss/base.scss";
 
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import HomeView from "@/views/HomeView.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  { path: "/", component: HomeView },
+  { path: "/about", component: () => import("./views/AboutView.vue") },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
 
 const app = createApp(App);
-
 app.use(router);
-
 app.mount("#app");
