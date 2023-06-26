c92893f127eba6cf801a4e702b6c8e32a8f15766
kwiatkowskiw <kwiatkowskiww@gmail.com>
2023-06-27 01:27:47 +0200

    create homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,29 @@
-<script setup lang="ts"></script>
-
 <template>
-  fdsfds
+  <div class="home">
+    <h2>Welcome to design Platzi Fake Store Api</h2>
+    <img class="home-image" src="../assets/img/home.png" alt="home-image"/>
+    <div class="home-items">
+      <div class="home-item">
+        <div class="item-header">
+          <img class="item-icon" src="../assets/img/product.svg" alt="icon-product"/>
+          <h3 class="item-title">Products</h3>
+        </div>
+        <span class="item-description">Get existing products. Select with categories</span>
+      </div>
+      <div class="home-item">
+        <div class="item-header">
+          <img class="item-icon" src="../assets/img/category.svg" alt="icon-category"/>
+          <h3 class="item-title">Categories</h3>
+        </div>
+        <span class="item-description">Get existing categories</span>
+      </div>
+      <div class="home-item">
+        <div class="item-header">
+          <img class="item-icon" src="../assets/img/user.svg" alt="icon-user"/>
+          <h3 class="item-title">Users</h3>
+        </div>
+        <span class="item-description">Get users to contact with specific user</span>
+      </div>
+    </div>
+  </div>
 </template>
