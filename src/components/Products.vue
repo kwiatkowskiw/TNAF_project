0e5c0a39e20f2eebe4f89e201ecf9ddf3d698a85
kwiatkowskiw <kwiatkowskiww@gmail.com>
2023-07-02 20:59:32 +0200

    edit filters

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,6 +4,7 @@ import ProductFilters from "@/components/ProductFilters.vue";
 import {useProductApi} from "@/composables/useProductApi";
 import {ref} from "vue";
 import type {IFilter, IProduct} from "../../types";
+import Pagination from "@/components/Pagination.vue";
 
 const {getProducts, getProductsByFilters} = useProductApi();
 
@@ -26,14 +27,18 @@ async function updateProduct(filters: IFilter) {
     <aside class="products-filters">
       <ProductFilters @search-products="updateProduct"/>
     </aside>
-    <section class="products-view" :class="loadingProducts && 'products-suspense-view'">
-      <span v-if="productResponse.length == 0" class="products-empty">No products with that filter</span>
-      <Product v-for="product in productResponse" :key="product.id"
-               :title="product.title"
-               :images="product.images"
-               :price="product.price"
-               :description="product.description">
-      </Product>
-    </section>
+    <span v-if="productResponse.length === 0" class="products-empty">No products with that filter</span>
+    <div v-else class="products-container"
+         :class="loadingProducts && 'products-suspense-view'">
+      <Pagination/>
+      <section class="products-view">
+        <Product v-for="product in productResponse" :key="product.id"
+                 :title="product.title"
+                 :images="product.images"
+                 :price="product.price"
+                 :description="product.description">
+        </Product>
+      </section>
+    </div>
   </div>
 </template>
